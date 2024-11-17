f5b319a0532d34b548ad19fb9b2ff00c9a7594e8
natali <NataliDegtyreva@yandex.ru>
2024-11-17 14:34:29 +0300

    feat: add loader

---
+++ components/layout/Loader.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="wrapper">
+  <img src="./Loader.vue" alt="Loader"/>
+</div>
+</template>
+
+<style scoped>
+.wrapper {
+  @apply bg-sidebar flex items-center justify-center w-screen h-screen;
+}
+</style>
